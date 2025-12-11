d149a817a2daa725a548f87302ce576d0c973bc1
alicjakr <krupczynskaalicja@gmail.com>
2025-12-11 18:22:34 +0100

    proper printing of data in tables and created functions later to be used for user's choice

--- f1dbfile.py
+++ f1dbfile.py
@@ -1,4 +1,5 @@
 import psycopg2
+from tabulate import tabulate
 
 conn=psycopg2.connect(database="template1",
                       host="localhost",
@@ -10,52 +11,53 @@ cursor=conn.cursor()
 cursor.execute(open("f1sql.sql", "r").read())
 conn.commit()
 
-cursor.execute("SELECT * FROM drivers ORDER BY drivers.current_points DESC")
-print("Drivers")
-rows=cursor.fetchall()
-for row in rows:
-    number, name, surname, team, current_points = row
-    print(f"{number}, {name}, {surname}, {team}, {current_points}")
+def drivers_table():
+    cursor.execute("SELECT * FROM drivers ORDER BY drivers.current_points DESC")
+    print("Drivers")
+    driver_rows=cursor.fetchall()
+    print(tabulate(driver_rows, headers=["Number", "Name", "Surname", "Team", "Current points"], tablefmt="fancy_grid"))
 
-cursor.execute("SELECT * FROM team_specifications ORDER BY team_specifications.constructor_points DESC")
-print("Team specifications")
-rows=cursor.fetchall()
-for row in rows:
-    team, engine_supplier, location, constructor_points, GP_points, GP_wins, GP_poles, sprint_points, sprint_wins, sprint_poles = row
-    print(f"{team}, {engine_supplier}, {location}, {constructor_points}, {GP_points}, {GP_wins}, {GP_poles}, {sprint_points}, {sprint_wins}, {sprint_poles}")
+def teams_table():
+    cursor.execute("SELECT * FROM team_specifications ORDER BY team_specifications.constructor_points DESC")
+    print("Team specifications")
+    team_rows=cursor.fetchall()
+    print(tabulate(team_rows, headers=["Team", "Engine supplier", "Location", "Constructor points", "GP points", "GP wins", "GP poles", "Sprint points", "Sprint wins", "Sprint poles"], tablefmt="fancy_grid"))
 
-cursor.execute("SELECT * FROM races")
-rows=cursor.fetchall()
-print("Races")
-for row in rows:
-    round_number, GP_name, GP_fullname, start_date, end_date, distance, circuit_length, laps_number = row
-    formatted_start_date=start_date.strftime("%Y-%m-%d")
-    formatted_end_date=end_date.strftime("%Y-%m-%d")
-    print(f"{round_number}, {GP_name}, {GP_fullname}, {formatted_start_date}, {formatted_end_date}, {distance}, {circuit_length}, {laps_number}")
+def races_table():
+    cursor.execute("SELECT * FROM races")
+    races_rows=cursor.fetchall()
+    print("Races")
+    print(tabulate(races_rows, headers=["Round number", "GP name", "GP full name", "Start date", "End date", "Distance", "Circuit length", "Laps number"], tablefmt="fancy_grid"))
 
-cursor.execute("SELECT * FROM fastest_lap_times")
-print("Fastest lap times")
-rows=cursor.fetchall()
-for row in rows:
-    track_name, GP_name, driver_number, driver_name, driver_surname, team, lap_time = row
-    minutes=lap_time.minute
-    seconds=lap_time.second
-    milliseconds=int(lap_time.microsecond / 1000)
-    formatted_time=f"{minutes}:{seconds}.{milliseconds}"
-    print(f"{track_name}, {GP_name}, {driver_number}, {driver_name}, {driver_surname}, {team}, {formatted_time}")
+def flts_table_():
+    cursor.execute("SELECT * FROM fastest_lap_times")
+    print("Fastest lap times")
+    flt_rows=cursor.fetchall()
+    frows=[]
+    for row in flt_rows:
+        track_name, GP_name, driver_number, driver_name, driver_surname, team, lap_time=row
+        fminutes=lap_time.minute
+        fseconds=lap_time.second
+        fmilliseconds=lap_time.microsecond//1000
+        flap_time=f"{fminutes:02d}:{fseconds:02d}.{fmilliseconds:03d}"
+        frows.append([track_name, GP_name, driver_number, driver_name, driver_surname, team, flap_time])
+    print(tabulate(frows, headers=["Track name", "GP name", "Driver's number", "Driver's name", "Driver's surname", "Team", "Time"], tablefmt="fancy_grid"))
 
-cursor.execute("SELECT * FROM race_results")
-print("Race results")
-rows=cursor.fetchall()
-for row in rows:
-    GP_name, race_date, winner_number, winner_name, winner_surname, team, laps_number, race_time = row
-    formatted_date=race_date.strftime("%Y-%m-%d")
-    hours=race_time.hour
-    minutes=race_time.minute
-    seconds=race_time.second
-    milliseconds=int(race_time.microsecond / 1000)
-    formatted_time=f"{hours}:{minutes}:{seconds}.{milliseconds}"
-    print(f"{GP_name}, {formatted_date}, {winner_number}, {winner_name}, {winner_surname}, {team}, {laps_number}, {formatted_time}")
+def rr_table():
+    cursor.execute("SELECT * FROM race_results")
+    print("Race results")
+    rr_rows=cursor.fetchall()
+    frrows=[]
+    for row in rr_rows:
+        GP_name, race_date, winner_number, winner_name, winner_surname, team, laps_number, race_time = row
+        formatted_date=race_date.strftime("%Y-%m-%d")
+        rhours=race_time.hour
+        rminutes=race_time.minute
+        rseconds=race_time.second
+        rmilliseconds=race_time.microsecond//1000
+        frace_time=f"{rhours:02d}:{rminutes:02d}:{rseconds:02d}.{rmilliseconds:03d}"
+        frrows.append([GP_name, race_date, winner_number, winner_name, winner_surname, team, laps_number, frace_time])
+    print(tabulate(frrows, headers=["GP name", "Date", "Winner's number", "Winner's name", "Winner's surname", "Team", "Laps' number", "Time"], tablefmt="fancy_grid"))
 
 cursor.close()
 conn.close()
