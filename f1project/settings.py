c9e8050b2c901a038989fe18c6e873bb068e5856
alicjakr <krupczynskaalicja@gmail.com>
2026-01-15 21:37:14 +0100

    completed v1

--- f1project/settings.py
+++ f1project/settings.py
@@ -74,17 +74,6 @@ WSGI_APPLICATION = 'f1project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-'''DATABASES={
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "template1",
-        "USER": "alicjakrupczynska",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}'''
-
 DATABASES={
     "default": {
         "ENGINE": "django.db.backends.postgresql",
