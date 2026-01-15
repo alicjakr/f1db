cce670263c0b6875cb38b6cfe9358ec83ac52d48
alicjakr <krupczynskaalicja@gmail.com>
2026-01-15 21:28:41 +0100

    completed v1

--- f1project/settings.py
+++ f1project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t#^m)i#rq=ssxkwd$01f^)usq2b985=qx)$3&(m8bl_i8p65pt'
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-key-for-development")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
